64a1f23c240fa973b1cfcbf8806e90915d539d07
Ashish Maharjan <amaharjan1032@gmail.com>
2021-04-14 16:20:38 +0545

    Delete done

--- src/App.js
+++ src/App.js
@@ -15,10 +15,20 @@ function App() {
       day: 'feb 3rd at 1:30pm'
     }
   ]);
+
+  //Delete task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+
   return (
     <div className="container">
       <Header />
-      <Tasks tasks={tasks} />
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} />
+      ) : (
+        'No tasks to show'
+      )}
     </div>
   );
 }
